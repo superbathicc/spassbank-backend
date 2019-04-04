9b4d9e18cd7ad17101c4229bfcb503ae83f51305
Kounah <kounalyn@gmail.com>
2019-04-04 13:22:34 +0200

    added source delivery

--- app/routes/docs.js
+++ app/routes/docs.js
@@ -1,11 +1,46 @@
 const path = require('path');
 const fs = require('fs');
+const filesize = require('filesize');
+const mime = require('mime');
 
 module.exports = function(app) {
   app.get('/', (req, res) => {
     res.render('index', {
       req: req
     });
+  });
+
+  app.get('/src/*', (req, res) => {
+    var rel = path.relative('/src/', req.path);
+    var p = path.join(__dirname, '../../src', rel);
+    if(fs.existsSync(p)) {
+      var stat = fs.statSync(p);
+      if(stat.isFile(p)) {
+        res.status(200).set('content-type', mime.getType(p)).sendFile(p);
+      } else {
+        res.render('internal/dir', {
+          req,
+          path: req.path,
+          items: fs.readdirSync(p).map(f => {
+            var stat = fs.statSync(path.join(p, f))
+            var type = "unknown";
+            if(stat.isFile()) type = "file";
+            if(stat.isDirectory()) type = 'dir';
+            if(stat.isSymbolicLink()) type = 'symlink';
+            if(stat.isSocket()) type = 'socket';
+            if(stat.isFIFO()) type = 'FIFO';
+            if(stat.isBlockDevice()) type = 'blockdevice';
+            if(stat.isCharacterDevice()) type = 'characterdevice';
+
+            var result = stat;
+            result.type = type;
+            result.name = f;
+            result.sizeText = filesize(stat.size);
+            return result;
+          })
+        })
+      }
+    }
   });
 
   app.get('/*', (req, res) => {
