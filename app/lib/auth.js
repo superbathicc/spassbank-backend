8190e85115fc12d17f5169641d5b47ae8c7aa806
Kounah <kounalyn@gmail.com>
2019-04-09 18:19:34 +0200

    generalized checkAuth functions

--- app/lib/auth.js
+++ app/lib/auth.js
@@ -23,111 +23,40 @@ function parseHeader(req) {
   }
 }
 
-async  function checkAccountAuth(req, res, next) {
-  try {
-    let auth = parseHeader(req);
-    if(typeof auth["Account"] == 'string') {
-      let account = await accountApi.getOneByHash(auth["Account"]);
-      if(account) {
-        req.session.account = account;
-        next()
+function getCheckAuthFunction(key, api) {
+  return async function(req, res, next) {
+    try {
+      let auth = parseHeader(req);
+      if(typeof auth[key] == 'string') {
+        let _ = await api.getOneByHash(auth[key]);
+        if(_) {
+          req.session[key] = _;
+          next()
+        } else {
+          res.sendStatus(401);
+        }
       } else {
         res.sendStatus(401);
       }
-    }
-  } catch(err) {
-    if(req.session.account) {
-      let account = await accountApi.getOneByHash(req.session.account.hash);
-      if(account) {
-        req.session.account = account;
-        next();
+    } catch(err) {
+      if(req.session[key]) {
+        let _ = await api.getOneByHash(req.session[key].hash);
+        if(admin) {
+          req.session[key] = _;
+          next();
+        } else {
+          res.sendStatus(401);
+        }
       } else {
         res.sendStatus(401);
       }
-    }
-  }
-}
-
-async function checkCustomerAuth(req, res, next) {
-  try {
-    let auth = parseHeader(req);
-    if(typeof auth["Customer"] == 'string') {
-      let customer = await customerApi.getOneByHash(auth["Customer"]);
-      if(customer) {
-        req.session.customer = customer;
-        next()
-      } else {
-        res.sendStatus(401);
-      }
-    }
-  } catch(err) {
-    if(req.session.customer) {
-      let customer = await customerApi.getOneByHash(req.session.customer.hash);
-      if(customer) {
-        req.session.customer = customer;
-        next();
-      } else {
-        res.sendStatus(401);
-      }
-    }
-  }
-}
-
-async function checkEmployeeAuth(req, res, next) {
-  try {
-    let auth = parseHeader(req);
-    if(typeof auth["Employee"] == 'string') {
-      let employee = await employeeApi.getOneByHash(auth["Employee"]);
-      if(employee) {
-        req.session.employee = employee;
-        next()
-      } else {
-        res.sendStatus(401);
-      }
-    }
-  } catch(err) {
-    if(req.session.employee) {
-      let employee = await employeeApi.getOneByHash(req.session.employee.hash);
-      if(employee) {
-        req.session.employee = employee;
-        next();
-      } else {
-        res.sendStatus(401);
-      }
-    }
-  }
-}
-
-async function checkAdminAuth(req, res, next) {
-  try {
-    let auth = parseHeader(req);
-    if(typeof auth["Employee"] == 'string') {
-      let admin = await adminApi.getOneByHash(auth["Employee"]);
-      if(admin) {
-        req.session.admin = admin;
-        next()
-      } else {
-        res.sendStatus(401);
-      }
-    }
-  } catch(err) {
-    if(req.session.admin) {
-      let admin = await adminApi.getOneByHash(req.session.admin.hash);
-      if(admin) {
-        req.session.admin = admin;
-        next();
-      } else {
-        res.sendStatus(401);
-      }
-    } else {
-      res.sendStatus(401);
     }
   }
 }
 
 module.exports = {
-  checkAccountAuth,
-  checkCustomerAuth,
-  checkEmployeeAuth,
-  checkAdminAuth
+  checkAccountAuth: getCheckAuthFunction("Account", accountApi),
+  checkCustomerAuth: getCheckAuthFunction("Customer", customerApi),
+  checkEmployeeAuth: getCheckAuthFunction("Employee", employeeApi),
+  checkAdminAuth: getCheckAuthFunction("Admin", adminApi)
 }
